784721c0230e5d596c8b48cc2aadb8d515324617
mzhussain0 <mzhussain092@gmail.com>
2023-12-05 13:03:02 +0530

    integrated payload cms

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,28 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
- 
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
+
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+export function formatPrice(
+  price: number | string,
+  options: {
+    currency?: "USD" | "INR";
+    notation?: Intl.NumberFormatOptions["notation"];
+  } = {}
+) {
+  const { currency = "INR", notation = "compact" } = options;
+  const numericPrice = typeof price === "string" ? parseFloat(price) : price;
+
+  const numberFormatOptions: Intl.NumberFormatOptions = {
+    style: "currency",
+    currency,
+    notation,
+    maximumFractionDigits: 2,
+  };
+
+  return new Intl.NumberFormat("en-IN", numberFormatOptions).format(
+    numericPrice
+  );
 }
